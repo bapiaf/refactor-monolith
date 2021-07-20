36fcd427b5c599e8c6adb2b6c50a5a839bdf755f
bapiaf <baptiste.iafrate@diabolocom.com>
2021-07-20 10:52:25 +0200

    changed reverseproxy url for new k8 deployment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   apiHost:
-    'http://a84b530ae370945dca00c945b721a4f3-2063780888.us-east-1.elb.amazonaws.com:8080/api/v0',
+    'http://ac7752ffa213747aa99e0fe8f30bcabc-2110885717.us-east-1.elb.amazonaws.com:8080/api/v0',
 };
 
 /*
